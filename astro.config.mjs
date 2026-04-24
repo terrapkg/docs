ea80f73eb537671a77ed07ca822f635b9758abfc
Lleyton Gray <lleyton@fyralabs.com>
2026-04-24 16:27:59 -0700

    chore: add wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,66 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import starlight from '@astrojs/starlight';
-import expressiveCode from 'astro-expressive-code';
-import icon from 'astro-icon';
-import mermaid from 'astro-mermaid';
+import starlight from "@astrojs/starlight";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
+import mermaid from "astro-mermaid";
 import tailwindcss from "@tailwindcss/vite";
-import RPMSpec from './components/spec.json';
-import rhai from './components/rhai.json'
+import RPMSpec from "./components/spec.json";
+import rhai from "./components/rhai.json";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://docs.terrapkg.com",
-    integrations: [
-      mermaid(),
-      starlight({
-        title: 'Terra Documentation',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/terrapkg' }],
-        sidebar: [
-            {
-                label: 'Index',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Page Index', slug: 'terra' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
-        ],
-        }),
-      sitemap(),
-      expressiveCode(),
-      icon(),
-      react(),
-      mdx()
-    ],
-    markdown: {
-      shikiConfig: {
-        langs: [
-          { ...RPMSpec, aliases: ["rpmspec"] },
-          { ...rhai, aliases: ["rhai"] }
-        ],
-	  },
+  site: "https://docs.terrapkg.com",
+
+  integrations: [
+    mermaid(),
+    starlight({
+      title: "Terra Documentation",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/terrapkg",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Index",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Page Index", slug: "terra" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+    sitemap(),
+    expressiveCode(),
+    icon(),
+    react(),
+    mdx(),
+  ],
+
+  markdown: {
+    shikiConfig: {
+      langs: [
+        { ...RPMSpec, aliases: ["rpmspec"] },
+        { ...rhai, aliases: ["rhai"] },
+      ],
     },
-    vite: {
-      plugins: [tailwindcss()],
-    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare(),
 });
