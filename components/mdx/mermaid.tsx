af83fa3ac36e0f95d2c66f83e09f1bb8ddb90fdf
Gilver <roachy@fyralabs.com>
2026-05-12 01:35:46 -0500

    Testing more

---
+++ components/mdx/mermaid.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { use, useEffect, useId, useState } from 'react';
+
+const htmlTheme = document.documentElement.getAttribute('data-theme');
+const bodyTheme = document.body.getAttribute('data-theme');
+const dataTheme = htmlTheme || bodyTheme;
+
+export function Mermaid({ chart }: { chart: string }) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return;
+    return <MermaidContent chart={chart} />;
+}
+
+const cache = new Map<string, Promise<unknown>>();
+
+function cachePromise<T>(key: string, setPromise: () => Promise<T>): Promise<T> {
+    const cached = cache.get(key);
+    if (cached) return cached as Promise<T>;
+
+    const promise = setPromise();
+    cache.set(key, promise);
+    return promise;
+}
+
+function MermaidContent({ chart }: { chart: string }) {
+    const id = useId();
+    const { default: mermaid } = use(cachePromise('mermaid', () => import('mermaid')));
+
+    mermaid.initialize({
+        startOnLoad: false,
+        securityLevel: 'loose',
+        fontFamily: 'inherit',
+        themeCSS: 'margin: 1.5rem auto 0;',
+        theme: dataTheme === 'dark' ? 'dark' : 'default',
+    });
+
+    const { svg, bindFunctions } = use(
+        cachePromise(`${chart}-${dataTheme}`, () => {
+            return mermaid.render(id, chart.replaceAll('\\n', '\n'));
+        }),
+    );
+
+    return (
+        <div
+        ref={(container) => {
+            if (container) bindFunctions?.(container);
+        }}
+        dangerouslySetInnerHTML={{ __html: svg }}
+        />
+    );
+}
