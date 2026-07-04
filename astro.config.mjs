ac6b3141b90b30a98554a73c0ecf6328b660349c
Gilver <roachy@fyralabs.com>
2026-07-04 00:37:44 -0500

    chore: Fix fmt

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,98 +26,98 @@ export default defineConfig({
     starlight({
       title: "Terra Documentation",
       defaultLocale: "en",
-        locales: {
-          root: {
-            label: "English",
-            lang: "en",
-          },
+      locales: {
+        root: {
+          label: "English",
+          lang: "en",
         },
-        social: [
-          {
-            icon: "github",
-            label: "GitHub",
-            href: "https://github.com/terrapkg",
-          },
-          {
-            icon: "discord",
-            label: "Fyra Labs Discord",
-            href: "https://fyralabs.com/discord",
-          },
-          {
-            icon: "blueSky",
-            label: "Bluesky",
-            href: "https://bsky.app/profile/terrapkg.bsky.social",
-          },
-          {
-            // It's Misskey but Starlight doesn't currently have an icon for this.
-            icon: "mastodon",
-            label: "Misskey",
-            href: "https://fedi.fyralabs.com/@terra",
-          },
-          {
-            icon: "twitter",
-            label: "Twitter",
-            href: "https://twitter.com/terra_repo",
-          },
-        ],
-        sidebar: [
-          {
-            label: "Welcome",
-            items: [{ label: "Introduction", slug: "index" }],
-          },
-          {
-            label: "Reference",
-            items: [{ autogenerate: { directory: "reference" } }],
-          },
-          {
-            label: "General",
-            items: [{ autogenerate: { directory: "general" } }],
-          },
-          {
-            label: "Using Terra",
-            items: [{ autogenerate: { directory: "usage" } }],
-          },
-          {
-            label: "Contributing",
-            items: [
-              { label: "Getting Started", slug: "contributing/getting-started" },
-              { label: "Policies", slug: "contributing/policies" },
-              {
-                label: "Guidelines",
-                items: [
-                  { label: "General Guidelines", slug: "contributing/guidelines" },
-                  {
-                    label: "Special Guidelines",
-                    items: ["contributing/appstream"],
-                  },
-                ],
-              },
-              { label: "Autoupdating Packages", slug: "contributing/autoupdate" },
-              { label: "Custom RPM Macros", slug: "contributing/srpm" },
-            ],
-          },
-        ],
-        lastUpdated: true,
-        editLink: {
-          baseUrl: "https://github.com/terrapkg/docs",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/terrapkg",
         },
-        expressiveCode: {
-          themes: ["github-dark"],
-          shiki: {
-            langs: [
-              // @ts-expect-error TypeScript wants you use JSON.parse(), despite this working perfectly fine.
-              { ...RPMSpec, aliases: ["rpmspec"] },
-              // @ts-expect-error TypeScript wants you use JSON.parse(), despite this working perfectly fine.
-              { ...rhai, aliases: ["rhai"] },
-            ],
-            // By default Shiki uses Oniguruma with WASM in Expressive Code, Cloudflare does not support this.
-            engine: "javascript",
-          },
+        {
+          icon: "discord",
+          label: "Fyra Labs Discord",
+          href: "https://fyralabs.com/discord",
         },
-        plugins:
+        {
+          icon: "blueSky",
+          label: "Bluesky",
+          href: "https://bsky.app/profile/terrapkg.bsky.social",
+        },
+        {
+          // It's Misskey but Starlight doesn't currently have an icon for this.
+          icon: "mastodon",
+          label: "Misskey",
+          href: "https://fedi.fyralabs.com/@terra",
+        },
+        {
+          icon: "twitter",
+          label: "Twitter",
+          href: "https://twitter.com/terra_repo",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Welcome",
+          items: [{ label: "Introduction", slug: "index" }],
+        },
+        {
+          label: "Reference",
+          items: [{ autogenerate: { directory: "reference" } }],
+        },
+        {
+          label: "General",
+          items: [{ autogenerate: { directory: "general" } }],
+        },
+        {
+          label: "Using Terra",
+          items: [{ autogenerate: { directory: "usage" } }],
+        },
+        {
+          label: "Contributing",
+          items: [
+            { label: "Getting Started", slug: "contributing/getting-started" },
+            { label: "Policies", slug: "contributing/policies" },
+            {
+              label: "Guidelines",
+              items: [
+                { label: "General Guidelines", slug: "contributing/guidelines" },
+                {
+                  label: "Special Guidelines",
+                  items: ["contributing/appstream"],
+                },
+              ],
+            },
+            { label: "Autoupdating Packages", slug: "contributing/autoupdate" },
+            { label: "Custom RPM Macros", slug: "contributing/srpm" },
+          ],
+        },
+      ],
+      lastUpdated: true,
+      editLink: {
+        baseUrl: "https://github.com/terrapkg/docs",
+      },
+      expressiveCode: {
+        themes: ["github-dark"],
+        shiki: {
+          langs: [
+            // @ts-expect-error TypeScript wants you use JSON.parse(), despite this working perfectly fine.
+            { ...RPMSpec, aliases: ["rpmspec"] },
+            // @ts-expect-error TypeScript wants you use JSON.parse(), despite this working perfectly fine.
+            { ...rhai, aliases: ["rhai"] },
+          ],
+          // By default Shiki uses Oniguruma with WASM in Expressive Code, Cloudflare does not support this.
+          engine: "javascript",
+        },
+      },
+      plugins:
         process.env.CHECK_LINKS === "true"
-        ? [starlightLinksValidator({ errorOnFallbackPages: false })]
-        : [],
+          ? [starlightLinksValidator({ errorOnFallbackPages: false })]
+          : [],
     }),
     react(),
   ],
