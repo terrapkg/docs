a229db549cf42df916ace7ef05f6cb5c52b1ccb1
Owen-sz <owen@fyralabs.com>
2026-07-22 23:58:05 -0500

    chore: contain rust docs to one part
    Signed-off-by: Owen-sz <owen@fyralabs.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -109,9 +109,6 @@ export default defineConfig({
           ],
         },
         {
-          label: "Guides",
-          items: [{ label: "Rust", slug: "guide/rust" }],
-        },
         {
           label: "Tuatara",
           items: [
